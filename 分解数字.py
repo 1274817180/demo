1ac720d65efab5d3d50ab300f7f2d66faa840734
马腾 <929987833@qq.com>
2019-12-05 16:57:32 +0800

    完成

--- 分解数字.py
+++ 分解数字.py
@@ -28,3 +28,21 @@ def resolve(num):
 
 
 print(resolve(18))
+
+
+
+                pass
+    elif num % 2 == 1:
+        num = num - 1
+        for i in li:
+            if num >= i:
+                li1.append(i)
+                num = num - i
+            elif num == 0:
+                li1.append(1)
+                return li1
+            else:
+                pass
+
+
+print(resolve(18))
